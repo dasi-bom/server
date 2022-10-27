2577b64f6db96e4038e0b244cb78ec81bb641a68
minju412 <ln8847@ajou.ac.kr>
2022-10-27 22:49:25 +0900

    initial

---
+++ service-discovery/src/test/java/com/dasibom/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasibom.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
